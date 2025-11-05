42987a92face766b1fc6ab7820ed4abc9ac2a643
hrishikeshkhandagale <hrishikeshkhandagale2@gmail.com>
2025-11-05 17:39:03 +0530

    modified

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'); // Imports the Express framework
+const app = express();              // Creates an Express application
+const port = 3000;                  // Defines the port the app will listen on
 
+// Defines a route for GET requests to the root URL ('/')
 app.get('/', (req, res) => {
-  res.send('Hello from jenkins, added webhook, this is the third version of rushi');
+  res.send('Hello from jenkins, added webhook, this is the third version of z');
 });
 
+// Starts the server and logs a message to the console
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
